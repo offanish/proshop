d6edc8840fe0a3485c3fec247859f355f9a36ef4
Anish Gautam <off.anish@gmail.com>
2023-02-12 13:26:55 +0545

    created a basic server

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
+
 const App = () => {
-  return <h1>Hello World</h1>
+  return (
+    <BrowserRouter>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/product/:id' element={<ProductPage />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  )
 }
 
 export default App
