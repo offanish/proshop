32aea592b4af4122f61b52a44952d1cb5cbbf1b8
Anish Gautam <off.anish@gmail.com>
2023-02-20 15:58:55 +0545

    get users backend admin

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,14 +3,15 @@ const router = express.Router()
 
 import {
   getUserProfile,
+  getUsers,
   loginUser,
   registerUser,
   updateUserProfile,
 } from '../controllers/userController.js'
 
-import protect from '../middleware/authMiddleware.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
-router.route('/').post(registerUser)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.route('/login').post(loginUser)
 router
   .route('/profile')
